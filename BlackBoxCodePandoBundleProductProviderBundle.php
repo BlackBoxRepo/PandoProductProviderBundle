14af5883f01517021e1bdff467bf5f862b536b04
Christopher West <9ninemind@gmail.com>
2015-04-04 15:00:40 -0600

    creates bundle

---
+++ BlackBoxCodePandoBundleProductProviderBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BlackBoxCode\Pando\Bundle\ProductProviderBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BlackBoxCodePandoBundleProductProviderBundle extends Bundle
+{
+}
